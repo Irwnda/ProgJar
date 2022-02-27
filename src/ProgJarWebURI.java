4cc353a2ea9e2bceb6ff679b960ba4b5751a121a
Akbar Irwanda <irwandaa7@gmail.com>
2022-02-27 20:48:21 +0700

    add readability

--- src/ProgJarWebURI.java
+++ src/ProgJarWebURI.java
@@ -8,29 +8,44 @@ public class ProgJarWebURI {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter valid URL\t: ");
-        String[] inputURL = sc.nextLine().split("/", 2);
+        String input = sc.nextLine();
+        String[] inputURL;
+        String protocol;
         sc.close();
 
-        String server = inputURL[0];
-        String path = ("/" + inputURL[1]);
+        if (input.indexOf("https://") != -1)
+            protocol = "https";
+        else
+            protocol = "http";
 
-        Socket socket = new Socket(server, 80);
+        if (input.indexOf("://") != -1) {
+            input = input.substring(input.indexOf("://") + 3);
+        }
+
+        inputURL = input.split("/", 2);
+        String server = inputURL[0];
+        String path = inputURL.length == 1 ? "/" : ("/" + inputURL[1]);
+
+        Socket socket = new Socket(server, protocol == "http" ? 80 : 443);
 
         BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
         BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
 
-        bos.write(("GET " + path + " HTTP/1.1\r\nHost: " + server + "\r\n\r\n").getBytes());
+        bos.write(("GET " + path + " " + protocol.toUpperCase() + "/1.1\r\nHost: " + server + "\r\n\r\n").getBytes());
         bos.flush();
 
         byte[] c = bis.readAllBytes();
 
+        System.out.println(new String(c));
+        System.out.println("==============================");
         showLinks(new String(c));
+        System.out.println("==============================");
 
         socket.close();
     }
 
     private static void showLinks(String response) {
-        ArrayList < ArrayList < Object >> anchors = new ArrayList < ArrayList < Object >> ();
+        ArrayList<ArrayList<Object>> anchors = new ArrayList<ArrayList<Object>>();
         Scanner resp = new Scanner(response);
         boolean isClosed = true;
 
@@ -41,8 +56,10 @@ public class ProgJarWebURI {
             if (anchorIndex != -1) {
                 while (isClosed && anchorIndex != -1) {
                     line = line.substring(anchorIndex);
-                    ArrayList < Object > anchor = getLink(line);
-                    anchors.add(anchor);
+                    ArrayList<Object> anchor = getLink(line);
+
+                    if (validateLink(anchor))
+                        anchors.add((anchor));
 
                     isClosed = (boolean) anchor.get(2);
 
@@ -51,16 +68,20 @@ public class ProgJarWebURI {
             }
 
         }
+
         System.out.println("\nClickable links:");
-        for (int i = 0; i < anchors.size(); i++) {
-            System.out.println((i + 1) + ".\t" + anchors.get(i).get(0));
-            System.out.println("\t" + anchors.get(i).get(1) + "\n");
-        }
+        if (anchors.size() == 0)
+            System.out.println("1.\t-");
+        else
+            for (int i = 0; i < anchors.size(); i++) {
+                System.out.println((i + 1) + ".\t" + anchors.get(i).get(0));
+                System.out.println("\t" + anchors.get(i).get(1) + "\n");
+            }
         resp.close();
     }
 
-    private static ArrayList < Object > getLink(String line) {
-        ArrayList < Object > anchor = new ArrayList < Object > ();
+    private static ArrayList<Object> getLink(String line) {
+        ArrayList<Object> anchor = new ArrayList<Object>();
         int indexOfHref = line.indexOf("href=");
         int anchorURLFirst = line.indexOf("\"", indexOfHref);
         int anchorURLLast = line.indexOf("\"", anchorURLFirst + 1);
@@ -72,9 +93,9 @@ public class ProgJarWebURI {
 
         boolean closed = anchorTextLast != -1;
         if (closed) {
-            anchor.add(line.substring(anchorTextFirst + 1, anchorTextLast));
+            anchor.add(getTextOnly(line.substring(anchorTextFirst + 1, anchorTextLast)));
         } else {
-            anchor.add(line.substring(anchorTextFirst + 1));
+            anchor.add(getTextOnly(line.substring(anchorTextFirst + 1)));
         }
         anchor.add(closed);
         anchor.add(anchorTextLast);
@@ -82,4 +103,24 @@ public class ProgJarWebURI {
         return anchor;
     }
 
+    // This is to exclude anchor tag that refer to same page (like '#')
+    private static boolean validateLink(ArrayList<Object> linkInfo) {
+        return (linkInfo.get(0)).toString().indexOf(".") != -1;
+    }
+
+    // This is to delete html tag like header, paragraph, div, etc.
+    private static String getTextOnly(String linkInfo) {
+        String TextOnly = linkInfo;
+        if (TextOnly.indexOf("<") != -1) {
+            int gtSign = TextOnly.indexOf(">");
+            int ClosingTag = TextOnly.indexOf("</");
+            if (ClosingTag == -1)
+                TextOnly = TextOnly.substring(gtSign + 1);
+            else
+                TextOnly = TextOnly.substring(gtSign + 1, ClosingTag);
+        }
+
+        return TextOnly.length() > 0 ? TextOnly.trim() : linkInfo;
+        // This is to show the element inside anchor tag if there is only one element.
+    }
 }
